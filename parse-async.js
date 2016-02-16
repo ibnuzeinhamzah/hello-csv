15febfb9bd38403b8f994f7f5dc357e9cccd5f1b
Ibnu Zein Hamzah <ibnuzeinhamzah@gmail.com>
2016-02-17 01:03:25 +0700

    Update parse-async.js
    add function for create stream from read file

--- parse-async.js
+++ parse-async.js
@@ -1,1 +1,12 @@
 // Please use async lib https://github.com/caolan/async
+'use strict';
+
+const debug = require('debug')('hello');
+const fs = require('fs');
+const Q = require('q');
+
+exports.loadFile = function (file, cb) {
+    let deferred = Q.defer();
+    deferred.resolve(fs.createReadStream(file));
+    return deferred.promise;
+};
